890af4cb67010122191f6ad7b393331400ab5740
Денис Хмыров <denis@Deniss-MacBook-Pro.local>
2024-10-27 21:49:59 +0300

    fix: binary search moved to seach-algorithms

---
+++ search.js
@@ -0,0 +1,19 @@
+function binarySearch(array, valueToFind) {
+    let minIndex = 0;
+    let maxIndex = array.length - 1;
+    while (minIndex <= maxIndex) {
+        let midIndex = Math.floor((maxIndex + minIndex) / 2);
+        let guess = array[midIndex];
+        
+        if (guess === valueToFind) {
+            return `искомое значение находится на позиции ${midIndex}`;
+        }
+        if (guess < valueToFind) {
+            minIndex = midIndex + 1;
+        } else {
+            maxIndex = midIndex - 1;
+        }
+    }
+    return `искомое значение ${valueToFind} отсутствует в массиве`;
+}
+
